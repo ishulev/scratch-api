c83c8fbd2f8fd316dda84f23df6234f1b0f21bc3
ishulev <ivan.venelinov.shulev@gmail.com>
2021-08-18 16:39:10 +0300

    Watching files (Failed)

--- watcher.js
+++ watcher.js
@@ -1,15 +1,46 @@
 const { watch } = require("fs");
-const { execSync } = require('child_process')
+const { spawn } = require("child_process");
+const { kill } = require("process");
+
+let serveProcess, compileProcess;
+
+function compileAndServe() {
+  compileProcess = spawn("yarn", ["run", "compile"]);
+  compileProcess.on("close", function (code) {
+    if (code === 0) {
+      if (serveProcess) {
+        console.log(serveProcess.killed);
+        // serveProcess.stdin.pause();
+        // serveProcess.stdin.end();
+        // spawn("sh", ["-c", "kill -INT -" + serveProcess.pid]);
+        serveProcess.kill();
+      } else {
+        serveProcess = spawn("node", ["dist/index.js"]);
+      }
+      console.log(serveProcess.pid);
+      serveProcess.on("close", function (code, signal) {
+        console.log("CODE => " + code);
+        console.log("SIGNAL => " + signal);
+        serveProcess = spawn("node", ["dist/index.js"]);
+      });
+      serveProcess.stderr.on("data", (data) => {
+        console.error(`grep stderr: ${data}`);
+      });
+    }
+  });
+}
 
 let timeout = null;
 
-watch("./src", function(event, filename) {
-    if(timeout) {
-        clearTimeout(timeout);
-    }
-    // For some reason the watcher fires twice in a row
-    timeout = setTimeout(() => {
-        console.log("compiling!");
-        execSync("yarn compile");
-    }, 100);
-})
+compileAndServe();
+
+watch("./src", function (event, filename) {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+  // For some reason the watcher fires twice in a row
+  timeout = setTimeout(() => {
+    console.log("recompiling");
+    compileAndServe();
+  }, 100);
+});

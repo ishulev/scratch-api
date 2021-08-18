3df3b8b212879f51c104987d76d30872bc5dac00
ishulev <ivan.venelinov.shulev@gmail.com>
2021-08-18 11:40:29 +0300

    Handle duplicate events

--- watcher.js
+++ watcher.js
@@ -1,6 +1,15 @@
 const { watch } = require("fs");
 const { execSync } = require('child_process')
 
+let timeout = null;
+
 watch("./src", function(event, filename) {
-    execSync("yarn compile");
+    if(timeout) {
+        clearTimeout(timeout);
+    }
+    // For some reason the watcher fires twice in a row
+    timeout = setTimeout(() => {
+        console.log("compiling!");
+        execSync("yarn compile");
+    }, 100);
 })
